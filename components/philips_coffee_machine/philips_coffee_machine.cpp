7581d7b7ef3fe5274cc533b60f8bb8a400cc6d88
Roman Lipicer <roman@lipicer.com>
2024-08-10 10:12:53 +0200

    test2

--- components/philips_coffee_machine/philips_coffee_machine.cpp
+++ components/philips_coffee_machine/philips_coffee_machine.cpp
@@ -29,7 +29,7 @@ namespace esphome
                 uint8_t size = std::min(display_uart_.available(), DISPLAY_BUFFER_SIZE);
                 display_uart_.read_array(display_buffer, size);
 
-                ESP_LOGCONFIG(display_buffer, "Philips Coffee Display to ");
+                ESP_LOGCONFIG(*display_buffer, "Philips Coffee Display to ");
 
                 // Check if a action button is currently performing a long press
                 bool long_pressing = false;
